f98c380b0ff92a2034eec0bea38e0c5781af4e99
TjaLfE <heine@radeon.dk>
2014-10-17 16:39:19 +0200

    Hounddawgs

---
+++ sickbeard/providers/hounddawgs.py
@@ -0,0 +1,282 @@
+# Author: seedboy
+# URL: https://github.com/seedboy
+#
+# This file is part of SickRage.
+#
+# SickRage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SickRage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SickRage.  If not, see <http://www.gnu.org/licenses/>.
+
+import traceback
+import urllib2
+import urllib
+import time
+import re
+import datetime
+import urlparse
+import sickbeard
+import generic
+from sickbeard.common import Quality, cpu_presets
+from sickbeard import logger
+from sickbeard import tvcache
+from sickbeard import db
+from sickbeard import classes
+from sickbeard import helpers
+from sickbeard import show_name_helpers
+from sickbeard.common import Overview
+from sickbeard.exceptions import ex
+from sickbeard import clients
+from lib import requests
+from lib.requests import exceptions
+from sickbeard.bs4_parser import BS4Parser
+from sickbeard.helpers import sanitizeSceneName
+
+
+class NextGenProvider(generic.TorrentProvider):
+    urls = {'base_url': 'http://192.99.10.104/',
+            'search': 'http://192.99.10.104/torrents.php?type=&userid=&searchstr=%s&searchimdb=&searchtags=&order_by=s3&order_way=desc&%s',
+            'login_page': 'http://192.99.10.104/login.php',
+            'detail': 'https://nxtgn.org/details.php?id=%s',
+            'download': 'https://nxtgn.org/download.php?id=%s',
+            'takelogin': 'https://nxtgn.org/takelogin.php?csrf=',
+    }
+
+    def __init__(self):
+
+        generic.TorrentProvider.__init__(self, "HoundDawgs")
+
+        self.supportsBacklog = True
+
+        self.enabled = False
+        self.username = None
+        self.password = None
+        self.ratio = None
+
+        self.cache = HoundDawgsCache(self)
+
+        self.url = self.urls['base_url']
+
+        self.categories = '&filter_cat[58]=1&filter_cat[57]=1&filter_cat[74]=1'
+
+        self.last_login_check = None
+
+        self.login_opener = None
+
+    def isEnabled(self):
+        return self.enabled
+
+    def imageName(self):
+        return 'nextgen.png'
+
+    def getQuality(self, item, anime=False):
+
+        quality = Quality.sceneQuality(item[0], anime)
+        return quality
+
+    def getLoginParams(self):
+        return {
+            'username': self.conf('username'),
+            'password': self.conf('password'),
+            'login': 'Login',
+        }
+
+    def loginSuccess(self, output):
+        return '/logout.php' in output.lower()
+
+    def _get_season_search_strings(self, ep_obj):
+
+        search_string = {'Season': []}
+        for show_name in set(show_name_helpers.allPossibleShowNames(self.show)):
+            if ep_obj.show.air_by_date or ep_obj.show.sports:
+                ep_string = show_name + ' ' + str(ep_obj.airdate).split('-')[0]
+            elif ep_obj.show.anime:
+                ep_string = show_name + ' ' + "%d" % ep_obj.scene_absolute_number
+            else:
+                ep_string = show_name + ' S%02d' % int(ep_obj.scene_season)  #1) showName SXX
+
+            search_string['Season'].append(ep_string)
+
+        return [search_string]
+
+    def _get_episode_search_strings(self, ep_obj, add_string=''):
+
+        search_string = {'Episode': []}
+
+        if not ep_obj:
+            return []
+
+        if self.show.air_by_date:
+            for show_name in set(show_name_helpers.allPossibleShowNames(self.show)):
+                ep_string = sanitizeSceneName(show_name) + ' ' + \
+                            str(ep_obj.airdate).replace('-', '|')
+                search_string['Episode'].append(ep_string)
+        elif self.show.sports:
+            for show_name in set(show_name_helpers.allPossibleShowNames(self.show)):
+                ep_string = sanitizeSceneName(show_name) + ' ' + \
+                            str(ep_obj.airdate).replace('-', '|') + '|' + \
+                            ep_obj.airdate.strftime('%b')
+                search_string['Episode'].append(ep_string)
+        elif self.show.anime:
+            for show_name in set(show_name_helpers.allPossibleShowNames(self.show)):
+                ep_string = sanitizeSceneName(show_name) + ' ' + \
+                            "%i" % int(ep_obj.scene_absolute_number)
+                search_string['Episode'].append(ep_string)
+        else:
+            for show_name in set(show_name_helpers.allPossibleShowNames(self.show)):
+                ep_string = show_name_helpers.sanitizeSceneName(show_name) + ' ' + \
+                            sickbeard.config.naming_ep_type[2] % {'seasonnumber': ep_obj.scene_season,
+                                                                  'episodenumber': ep_obj.scene_episode}
+
+                search_string['Episode'].append(re.sub('\s+', ' ', ep_string))
+
+        return [search_string]
+
+    def _doSearch(self, search_params, search_mode='eponly', epcount=0, age=0):
+
+        results = []
+        items = {'Season': [], 'Episode': [], 'RSS': []}
+
+        if not self._doLogin():
+            return []
+
+        for mode in search_params.keys():
+
+            for search_string in search_params[mode]:
+
+                searchURL = self.urls['search'] % (search_string, self.categories)
+                logger.log(u"" + self.name + " search page URL: " + searchURL, logger.DEBUG)
+
+                data = self.getURL(searchURL)
+                if not data:
+                    continue
+
+                try:
+                    with BS4Parser(data.decode('iso-8859-1'), features=["html5lib", "permissive"]) as html:
+                        resultsTable = html.find('div', attrs={'id': 'torrent-table-wrapper'})
+
+                        if not resultsTable:
+                            logger.log(u"The Data returned from " + self.name + " do not contains any torrent",
+                                       logger.DEBUG)
+                            continue
+
+                        # Collecting entries
+                        entries_std = html.find_all('div', attrs={'id': 'torrent-std'})
+                        entries_sticky = html.find_all('div', attrs={'id': 'torrent-sticky'})
+
+                        entries = entries_std + entries_sticky
+
+                        #Xirg STANDARD TORRENTS
+                        #Continue only if one Release is found
+                        if len(entries) > 0:
+
+                            for result in entries:
+
+                                try:
+                                    torrentName = \
+                                    ((result.find('div', attrs={'id': 'torrent-udgivelse2-users'})).find('a'))['title']
+                                    torrentId = (
+                                    ((result.find('div', attrs={'id': 'torrent-download'})).find('a'))['href']).replace(
+                                        'download.php?id=', '')
+                                    torrent_name = str(torrentName)
+                                    torrent_download_url = (self.urls['download'] % torrentId).encode('utf8')
+                                    torrent_details_url = (self.urls['detail'] % torrentId).encode('utf8')
+                                    #torrent_seeders = int(result.find('div', attrs = {'id' : 'torrent-seeders'}).find('a')['class'][0])
+                                    ## Not used, perhaps in the future ##
+                                    #torrent_id = int(torrent['href'].replace('/details.php?id=', ''))
+                                    #torrent_leechers = int(result.find('td', attrs = {'class' : 'ac t_leechers'}).string)
+                                except (AttributeError, TypeError):
+                                    continue
+
+                                # Filter unseeded torrent and torrents with no name/url
+                                #if mode != 'RSS' and torrent_seeders == 0:
+                                #    continue
+
+                                if not torrent_name or not torrent_download_url:
+                                    continue
+
+                                item = torrent_name, torrent_download_url
+                                logger.log(u"Found result: " + torrent_name + " (" + torrent_details_url + ")",
+                                           logger.DEBUG)
+                                items[mode].append(item)
+
+                        else:
+                            logger.log(u"The Data returned from " + self.name + " do not contains any torrent",
+                                       logger.WARNING)
+                            continue
+
+                except Exception, e:
+                    logger.log(u"Failed parsing " + self.name + " Traceback: " + traceback.format_exc(),
+                               logger.ERROR)
+
+            results += items[mode]
+
+        return results
+
+    def _get_title_and_url(self, item):
+
+        title, url = item
+
+        if title:
+            title = u'' + title
+            title = title.replace(' ', '.')
+
+        if url:
+            url = str(url).replace('&amp;', '&')
+
+        return title, url
+
+    def findPropers(self, search_date=datetime.datetime.today()):
+
+        results = []
+
+        myDB = db.DBConnection()
+        sqlResults = myDB.select(
+            'SELECT s.show_name, e.showid, e.season, e.episode, e.status, e.airdate FROM tv_episodes AS e' +
+            ' INNER JOIN tv_shows AS s ON (e.showid = s.indexer_id)' +
+            ' WHERE e.airdate >= ' + str(search_date.toordinal()) +
+            ' AND (e.status IN (' + ','.join([str(x) for x in Quality.DOWNLOADED]) + ')' +
+            ' OR (e.status IN (' + ','.join([str(x) for x in Quality.SNATCHED]) + ')))'
+        )
+
+        if not sqlResults:
+            return []
+
+        for sqlshow in sqlResults:
+            self.show = helpers.findCertainShow(sickbeard.showList, int(sqlshow["showid"]))
+            if self.show:
+                curEp = self.show.getEpisode(int(sqlshow["season"]), int(sqlshow["episode"]))
+                searchString = self._get_episode_search_strings(curEp, add_string='PROPER|REPACK')
+
+                for item in self._doSearch(searchString[0]):
+                    title, url = self._get_title_and_url(item)
+                    results.append(classes.Proper(title, url, datetime.datetime.today(), self.show))
+
+        return results
+
+    def seedRatio(self):
+        return self.ratio
+
+
+class HoundDawgsCache(tvcache.TVCache):
+    def __init__(self, provider):
+
+        tvcache.TVCache.__init__(self, provider)
+
+        # Only poll NextGen every 10 minutes max
+        self.minTime = 10
+
+    def _getRSSData(self):
+        search_params = {'RSS': ['']}
+        return self.provider._doSearch(search_params)
+
+
+provider = NextGenProvider()
