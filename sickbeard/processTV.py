69ebafee347cffde9b709f2a7458ad6ea6404cf7
CJ Natan <cjnatan@users.noreply.github.com>
2014-10-21 01:13:51 +1300

    Update processTV.py
    Currently processTV.py script, doesn't delete remaining of files post extraction.
    
    The added code is targeting process_method copy.
    
    [I'll create the config change for menu and submit that also once done so users could opt out deletion of remaining files if thats not desired]
    
    Let me know if you have any feedback:)

--- sickbeard/processTV.py
+++ sickbeard/processTV.py
@@ -111,7 +111,9 @@ def processDir(dirName, nzbName=None, process_method=None, force=False, is_prior
     type: Type of postprocessing auto or manual
     """
 
-    global process_result, returnStr
+    global process_result, returnStr, unPACK_CleanUP
+
+    unPACK_CleanUP = []
 
     returnStr = ''
 
@@ -222,10 +224,46 @@ def processDir(dirName, nzbName=None, process_method=None, force=False, is_prior
                     if delete_folder(processPath, check_empty=False):
                         returnStr += logHelper(u"Deleted folder: " + processPath, logger.DEBUG)
 
+    #added by CJ 20141021 
+    if (process_method == "copy"):
+        returnStr += logHelper(u"Starting with cleanup of unwanted remaining files from unpacking: %s " % unPACK_CleanUP, logger.DEBUG)
+        for file_to_be_cleaned in unPACK_CleanUP:
+        #returnStr += logHelper(u"CLEANIP FILE: %s " % file_to_be_cleaned, logger.DEBUG)
+                deleteunPackedFiles(file_to_be_cleaned)
+    else:
+        returnStr += logHelper(u"Skipping cleanup of unwanted files from unpacking, method selected is not 'copy'. [%s" % str(process_method) + "]"
+            ,logger.DEBUG)
+
     if process_result:
         returnStr += logHelper(u"Successfully processed")
     else:
         returnStr += logHelper(u"Problem(s) during processing", logger.WARNING)
+
+    return returnStr
+
+#start
+#Added by CJ 20141021
+#Clean up files function [slight change from delete_files]
+def deleteunPackedFiles(unpackedfilename):
+    global returnStr
+#to be added at a later stage: additional config in GUI and allow users to opt out
+    returnStr += logHelper(u"Checking if unpacked file %s " % unpackedfilename + "exists: %s " % os.path.isfile(unpackedfilename),logger.DEBUG)
+    if os.path.isfile(unpackedfilename):#if file exists... try to delete
+            returnStr += logHelper(u"Deleting unpacked file: %s " % unpackedfilename)
+            try:
+                ek.ek(os.remove, unpackedfilename)
+                returnStr += logHelper(u"File exists at source post deletion?: %s " % os.path.isfile(unpackedfilename),logger.DEBUG)
+                if os.path.isfile(unpackedfilename):
+                                 returnStr += logHelper(u"File %s " % unpackedfilename + "had not been deleted!",logger.WARNING)
+                else:
+                                 returnStr += logHelper(u"File %s " % unpackedfilename + "had been successfully deleted.")
+
+            except OSError, e:
+                returnStr += logHelper(u"Unable to delete file " + unpackedfilename + ': ' + str(e.strerror), logger.WARNING)
+                pass
+
+    else:
+            returnStr += logHelper(u"Skipping deletion of file: %s " % unpackedfilename + "as it does not appear to exist..")
 
     return returnStr
 
@@ -307,7 +345,7 @@ def validateDir(path, dirName, nzbNameOriginal, failed):
     return False
 
 def unRAR(path, rarFiles, force):
-    global process_result, returnStr
+    global process_result, returnStr, unPACK_CleanUP
 
     unpacked_files = []
 
@@ -344,6 +382,10 @@ def unRAR(path, rarFiles, force):
                 continue
 
         returnStr += logHelper(u"UnRar content: " + str(unpacked_files), logger.DEBUG)
+#added by CJ 20141021
+        for unrarcontentCLEANUP in unpacked_files:
+            unPACK_CleanUP += [str(path) + "/" + str(unrarcontentCLEANUP)]
+#END
 
     return unpacked_files
 
