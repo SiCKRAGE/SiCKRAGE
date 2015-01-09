65c0f06f0f16a5ea99f2c9d04b158307dfe8fb9f
Raphael Mutschler <info@raphaelmutschler.de>
2015-01-09 14:06:13 +0100

    include german quality namings

--- sickbeard/common.py
+++ sickbeard/common.py
@@ -210,24 +210,26 @@ class Quality:
         if checkName(["(pdtv|hdtv|dsr|tvrip).(xvid|x264|h.?264)"], all) and not checkName(["(720|1080)[pi]"], all) and\
                 not checkName(["hr.ws.pdtv.x264"], any):
             return Quality.SDTV
+        elif checkName(["videomann"], all) and not checkName(["(720|1080)[pi]"], all):
+            return Quality.SDTV
         elif checkName(["web.dl|webrip", "xvid|x264|h.?264"], all) and not checkName(["(720|1080)[pi]"], all):
             return Quality.SDTV
-        elif checkName(["(dvdrip|b[r|d]rip)(.ws)?.(xvid|divx|x264)"], any) and not checkName(["(720|1080)[pi]"], all):
+        elif checkName(["(dvdrip|ituneshd|blurayrip|b[r|d]rip)(.ws)?.(xvid|divx|x264)"], any) and not checkName(["(720|1080)[pi]"], all):
             return Quality.SDDVD
         elif checkName(["720p", "hdtv", "x264"], all) or checkName(["hr.ws.pdtv.x264"], any) and not checkName(
-                ["(1080)[pi]"], all):
+                ["(1080)[pi]"], all) or checkName(["videomann", "720p"], all):
             return Quality.HDTV
         elif checkName(["720p|1080i", "hdtv", "mpeg-?2"], all) or checkName(["1080[pi].hdtv", "h.?264"], all):
             return Quality.RAWHDTV
         elif checkName(["1080p", "hdtv", "x264"], all):
             return Quality.FULLHDTV
-        elif checkName(["720p", "web.dl|webrip"], all) or checkName(["720p", "itunes", "h.?264"], all):
+        elif checkName(["720p", "web.?dl|webrip|webhd(rip)?"], all) or checkName(["720p", "(webhd|itunes)", "(h|x).?264"], all) or checkName(["720p", "(webhd|itunes)", "avc"], all):
             return Quality.HDWEBDL
-        elif checkName(["1080p", "web.dl|webrip"], all) or checkName(["1080p", "itunes", "h.?264"], all):
+        elif checkName(["1080p", "web.?dl|webrip|webhd(rip)?"], all) or checkName(["1080p", "(webhd|itunes)", "(h|x).?264"], all):
             return Quality.FULLHDWEBDL
-        elif checkName(["720p", "bluray|hddvd|b[r|d]rip", "x264"], all):
+        elif checkName(["720p", "bluray|hddvd|b[r|d]rip", "(h|x)264"], all):
             return Quality.HDBLURAY
-        elif checkName(["1080p", "bluray|hddvd|b[r|d]rip", "x264"], all):
+        elif checkName(["1080p", "bluray|hddvd|b[r|d]rip", "(h|x)264"], all):
             return Quality.FULLHDBLURAY
         else:
             return Quality.UNKNOWN
