e4757c566205c4d8a7a74c9c793f6cc0d54d828c
daarcm <daarcm@gmail.com>
2015-03-13 19:31:57 +1100

    Update iptorrents.py to be abel to search SxEE episodes
    Added to following to line 48
                                sickbeard.config.naming_ep_type[0] % {'seasonnumber': ep_obj.scene_season,
                                                                      'episodenumber': ep_obj.scene_episode} + ' %s' % add_string

--- sickbeard/providers/iptorrents.py
+++ sickbeard/providers/iptorrents.py
@@ -145,7 +145,9 @@ class IPTorrentsProvider(generic.TorrentProvider):
                 ep_string = show_name_helpers.sanitizeSceneName(show_name) + ' ' + \
                             sickbeard.config.naming_ep_type[2] % {'seasonnumber': ep_obj.scene_season,
                                                                   'episodenumber': ep_obj.scene_episode} + ' %s' % add_string
-
+                            sickbeard.config.naming_ep_type[0] % {'seasonnumber': ep_obj.scene_season,
+                                                                  'episodenumber': ep_obj.scene_episode} + ' %s' % add_string
+                                                                  
                 search_string['Episode'].append(re.sub('\s+', ' ', ep_string))
 
         return [search_string]
