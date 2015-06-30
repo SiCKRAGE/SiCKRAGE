9bae4639e42f5bd1f8d5361cab54036c00c582bb
david <db@d1b.org>
2015-06-30 13:30:19 +1000

    Remove the ezrss provider.

--- sickbeard/providers/__init__.py
+++ sickbeard/providers/__init__.py
@@ -16,8 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with SickRage.  If not, see <http://www.gnu.org/licenses/>.
 
-__all__ = ['ezrss',
-           'womble',
+__all__ = ['womble',
            'btn',
            'thepiratebay',
            'kat',
--- sickbeard/search.py
+++ sickbeard/search.py
@@ -202,19 +202,6 @@ def pickBestResult(results, show):
         if show and cur_result.show is not show:
             continue
 
-        # filter out possible bad torrents from providers such as ezrss
-        if isinstance(cur_result, sickbeard.classes.SearchResult):
-            if cur_result.resultType == "torrent" and sickbeard.TORRENT_METHOD != "blackhole":
-                if not cur_result.url.startswith('magnet'):
-                    cur_result.content = cur_result.provider.getURL(cur_result.url)
-                    if not cur_result.content:
-                        continue
-        else:
-            if not cur_result.url.startswith('magnet'):
-                cur_result.content = cur_result.provider.getURL(cur_result.url)
-                if not cur_result.content:
-                    continue
-
         # build the black And white list
         if show.is_anime:
             if not show.release_groups.is_valid(cur_result):
