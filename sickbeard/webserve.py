ca1400880d88fff65b83f21d3cadf23bbb1887f3
echel0n <echel0n@github.com>
2015-11-10 13:06:21 -0800

    Testing possible unicode fixes.

--- sickbeard/webserve.py
+++ sickbeard/webserve.py
@@ -157,9 +157,9 @@ class BaseHandler(RequestHandler):
     startTime = 0.
 
     if sys.version_info > (3, 0):
-            __str__ = lambda x: x.__unicode__()
-        else:
-            __str__ = lambda x: unicode(x).encode('utf-8')
+        __str__ = lambda x: x.__unicode__()
+    else:
+        __str__ = lambda x: unicode(x).encode('utf-8')
 
     def __init__(self, *args, **kwargs):
         self.startTime = time.time()
