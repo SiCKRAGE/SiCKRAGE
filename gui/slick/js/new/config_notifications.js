5fa65bc58adc1d4240b82e3324170e1d09faeca0
Amadeus <amadeus@myself.com>
2015-11-15 19:21:15 -0800

    Added handlers for the ".enabler" class  (previously located in ../config.js)

--- gui/slick/js/new/config_notifications.js
+++ gui/slick/js/new/config_notifications.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
     $('#config-components').tabs();
+
+    $(".enabler").each(function () {
+        if (!$(this).prop('checked')) { $('#content_' + $(this).attr('id')).hide(); }
+    });
+
+    $(".enabler").click(function () {
+        if ($(this).prop('checked')) {
+            $('#content_' + $(this).attr('id')).fadeIn("fast", "linear");
+        } else {
+            $('#content_' + $(this).attr('id')).fadeOut("fast", "linear");
+        }
+    });
 });
