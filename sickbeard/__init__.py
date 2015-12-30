9ec9f0e1827d9334acf4887c8fba37d212655f29
bone <coder.bone@gmail.com>
2015-12-31 05:22:30 +1100

    DBConnection.checkDBVersion() was not returning an int as expected, this caused db_version to get set to 1 in sickbeard.db
    sickbeard/__init__.py will detect and repair the damaged database.

--- sickbeard/__init__.py
+++ sickbeard/__init__.py
@@ -1378,6 +1378,10 @@ def initialize(consoleLogging=True):
 
         # initialize the main SB database
         myDB = db.DBConnection()
+        if myDB.checkDBVersion() == 1:
+            # db has a damaged version number only
+            logging.warning("REPAIR DAMAGED DB VERSION NUMBER")
+            myDB.action("UPDATE db_version SET db_version = ?", [mainDB.MAX_DB_VERSION])
         db.upgradeDatabase(myDB, mainDB.InitialSchema)
 
         # initialize the cache database
