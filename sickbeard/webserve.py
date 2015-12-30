177995b8f52756be14a3fe2640a3f0c808f301e7
mpcaddy <github@mpcaddy.co.uk>
2015-12-30 13:27:26 +0000

    Added recursive_unicode which I forgot

--- sickbeard/webserve.py
+++ sickbeard/webserve.py
@@ -218,7 +218,7 @@ class WebHandler(BaseHandler):
         threading.currentThread().name = threadName
         try:
             kwargs = recursive_unicode({k: self.request.arguments[k][0] for k in self.request.arguments if len(self.request.arguments[k]) == 1})
-            kwargs.update({k: self.request.arguments[k] for k in self.request.arguments if len(self.request.arguments[k]) > 1})
+            kwargs.update(recursive_unicode({k: self.request.arguments[k] for k in self.request.arguments if len(self.request.arguments[k]) > 1}))
             return function(**kwargs)
         except Exception:
             logging.debug('Failed doing webui callback [{}]: {}'.format(self.request.uri, traceback.format_exc()))
