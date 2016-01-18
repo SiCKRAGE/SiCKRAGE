bf3aecb52b695d18327a8b4fe2f13c1cfd856969
echel0n <echel0n@github.com>
2016-01-18 14:47:57 -0800

    Added travis-ci build files to .gitignore file

--- .travis/run.sh
+++ .travis/run.sh
@@ -2,5 +2,12 @@
 set -e
 set -x
 
+git config --global user.email "sickrage.tv@gmail.com"
+git config --global user.name "echel0n"
+
+pip install --upgrade -r requirements/global.txt
+pip install --upgrade -r requirements/ssl.txt
+pip install --upgrade -r requirements/optional.txt
+
 chmod +x tests/*.py
 python -m unittest discover tests
