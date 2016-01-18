1ca8e96fdec25d6ab1d1e6453d0ba858aac55c69
echel0n <echel0n@github.com>
2016-01-18 12:06:37 -0800

    HOTFIX: Added in call to install required packages for tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -29,6 +29,7 @@ import unittest
 from configobj import ConfigObj
 
 import sickrage
+from requirements import install_reqs
 from sickrage.core.caches import tv_cache
 from sickrage.core.caches.name_cache import nameCache
 from sickrage.core.databases import Connection, cache_db, failed_db, main_db
@@ -49,6 +50,9 @@ from sickrage.providers import NewznabProvider, GenericProvider, NZBProvider, To
 threading.currentThread().setName('TESTS')
 
 socket.setdefaulttimeout(30)
+
+# install packages
+install_reqs()
 
 TESTALL = False
 TESTSKIPPED = ['test_issue_submitter', 'test_ssl_sni']
@@ -164,7 +168,6 @@ def _fake_specifyEP(self, season, episode):
 
 
 episode.TVEpisode.specifyEpisode = _fake_specifyEP
-
 
 # =================
 # test classes
