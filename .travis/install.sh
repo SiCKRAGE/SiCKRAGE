74a6bf3cfce549485a598dbcee6bf6eb951790f5
echel0n <echel0n@github.com>
2016-01-18 13:34:53 -0800

    Added special folder for travis-ci tests to contain custom install scripts for handling pip installs of packages

---
+++ .travis/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+set -x
+
+git config --global user.email "sickrage.tv@gmail.com"
+git config --global user.name "echel0n"
+
+pip install --upgrade -r ../requirements/global.txt
+pip install --upgrade -r ../requirements/ssl.txt
+pip install --upgrade -r ../requirements/optional.txt
