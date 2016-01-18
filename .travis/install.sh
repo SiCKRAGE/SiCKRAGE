ededc5fa43233e3b80ba7737397ef0d8320d5cf6
echel0n <echel0n@github.com>
2016-01-18 14:08:51 -0800

    Correct path mistake for travis-ci builds.

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,6 +5,6 @@ set -x
 git config --global user.email "sickrage.tv@gmail.com"
 git config --global user.name "echel0n"
 
-pip install --upgrade -r ../requirements/global.txt
-pip install --upgrade -r ../requirements/ssl.txt
-pip install --upgrade -r ../requirements/optional.txt
+pip install --upgrade -r requirements/global.txt
+pip install --upgrade -r requirements/ssl.txt
+pip install --upgrade -r requirements/optional.txt
