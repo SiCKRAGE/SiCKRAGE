5eaefcfc6b0da0b387c4e300103e774c8bcf1ffe
echel0n <echel0n@github.com>
2016-01-18 14:53:29 -0800

    Added pip install for configobj to travis-ci builds

--- .travis/run.sh
+++ .travis/run.sh
@@ -8,6 +8,7 @@ git config --global user.name "echel0n"
 pip install --upgrade -r requirements/global.txt
 pip install --upgrade -r requirements/ssl.txt
 pip install --upgrade -r requirements/optional.txt
+pip install --upgrade configobj
 
 chmod +x tests/*.py
 python -m unittest discover tests
