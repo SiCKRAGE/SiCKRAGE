c5e4a38f21b7ddb837c1320a9d44da432b776582
echel0n <sickrage.tv@gmail.com>
2016-02-05 14:54:51 -0500

    Updated travis-cl run script for tests.

--- .travis/run.sh
+++ .travis/run.sh
@@ -5,9 +5,9 @@ set -x
 git config --global user.email "sickrage.tv@gmail.com"
 git config --global user.name "echel0n"
 
-pip install --upgrade -r requirements/global.txt
-pip install --upgrade -r requirements/ssl.txt
-pip install --upgrade -r requirements/optional.txt
+pip install --upgrade -r requirements/requirements.txt -c requirements/constraints.txt
+pip install --upgrade -r requirements/ssl.txt -c requirements/constraints.txt
+pip install --upgrade -r requirements/optional.txt -c requirements/constraints.txt
 pip install --upgrade configobj
 
 chmod +x tests/*.py
