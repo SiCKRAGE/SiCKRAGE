3d7492240d16a61f3548782acd7337335e94349a
Maxence Maireaux <maxence@realstorm.fr>
2016-03-03 15:37:48 +0100

    correct travis

--- .travis/run.sh
+++ .travis/run.sh
@@ -5,9 +5,9 @@ set -x
 git config --global user.email "sickrage.tv@gmail.com"
 git config --global user.name "echel0n"
 
-pip install --upgrade -r sickrage/requirements/requirements.txt -c sickrage/requirements/constraints.txt
-pip install --upgrade -r sickrage/requirements/ssl.txt -c sickrage/requirements/constraints.txt
-pip install --upgrade -r sickrage/requirements/optional.txt -c sickrage/requirements/constraints.txt
+pip install --upgrade -r sickrage/requirements/requirements.txt
+pip install --upgrade -r sickrage/requirements/ssl.txt
+pip install --upgrade -r sickrage/requirements/optional.txt
 pip install --upgrade configobj
 
 chmod +x tests/*.py
