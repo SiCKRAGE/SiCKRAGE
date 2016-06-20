7c2b5e21cc18188f92f9d9da9fff76b9ac4ce9ae
echel0n <sickrage.tv@gmail.com>
2016-06-20 07:18:40 -0700

    HOTFIX: Corrected pathes in travis-cl scripts

--- .travis/run.sh
+++ .travis/run.sh
@@ -2,13 +2,10 @@
 set -e
 set -x
 
-git config --global user.email "sickrage.tv@gmail.com"
+git config --global user.email "echel0n@sickrage.ca"
 git config --global user.name "echel0n"
 
-pip install --upgrade -r sickrage/requirements/requirements.txt -c sickrage/requirements/constraints.txt
-pip install --upgrade -r sickrage/requirements/ssl.txt -c sickrage/requirements/constraints.txt
-pip install --upgrade -r sickrage/requirements/optional.txt -c sickrage/requirements/constraints.txt
-pip install --upgrade configobj
+pip install --upgrade -r sickrage/requirements.txt -c sickrage/constraints.txt
 
 chmod +x tests/*.py
 python -m unittest discover tests
