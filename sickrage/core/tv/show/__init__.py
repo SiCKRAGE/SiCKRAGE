24767932f2b280e0ea20c5190945b725008ff0ad
echel0n <echel0n@sickrage.ca>
2016-10-09 00:47:08 -0700

    new: v8.3.9 fix: Fixed issues with episode renamer

--- sickrage/core/tv/show/__init__.py
+++ sickrage/core/tv/show/__init__.py
@@ -417,7 +417,7 @@ class TVShow(object):
     def getAllEpisodes(self, season=None, has_location=False):
         results = []
         for x in [x['doc'] for x in MainDB().db.get_many('tv_episodes', self.indexerid, with_doc=True)]:
-            if season is not None and x['season'] != season:
+            if season and x['season'] != season:
                 continue
             if has_location and x['location'] == '':
                 continue
