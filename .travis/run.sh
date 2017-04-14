7a9f9c48983c75253134a636a41eb00bc9ea5b9d
gtosh4 <gtosh4@gmail.com>
2017-04-14 00:25:31 -0700

    Travis runs in git root so remove 'sickrage/' parent on pip arguments

--- .travis/run.sh
+++ .travis/run.sh
@@ -5,7 +5,7 @@ set -x
 git config --global user.email "echel0n@sickrage.ca"
 git config --global user.name "echel0n"
 
-pip install --upgrade -r sickrage/requirements.txt -c sickrage/constraints.txt
+pip install --upgrade -r requirements.txt -c constraints.txt
 
 chmod +x tests/*.py
 python -m unittest discover tests
