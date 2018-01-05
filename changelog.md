a98de616b63ecca4160de3fd0e07119acf7cc5e9
echel0n <echel0n@sickrage.ca>
2018-01-04 20:43:33 -0500

    Fixed issue #157 - Daily and Manual Search log formatting issue

--- changelog.md
+++ changelog.md
@@ -1,6 +1,7 @@
 # Changelog
 
-- * d67e35d - 2018-01-04: Release v9.2.89 
+- * 0e3b7c0 - 2018-01-04: Fixed issue #157 - Daily and Manual Search log formatting issue 
+- * 6d49779 - 2018-01-04: Release v9.2.89 
 - * 1206659 - 2018-01-04: Fix for connection issues to RARBG 
 - * 5a2f6c5 - 2018-01-04: Updated imdbpie requirement to v4.4.2 
 - * 3a38155 - 2018-01-04: Fixed __getitem__ error for managing episode statues 
--- sickrage/core/search.py
+++ sickrage/core/search.py
@@ -174,14 +174,14 @@ def pickBestResult(results, show):
         if hasattr(cur_result.provider, 'minseed') and cur_result.seeders not in (-1, None):
             if int(cur_result.seeders) < int(cur_result.provider.minseed):
                 sickrage.app.log.info("Discarding torrent because it doesn't meet the"
-                                      " minimum seeders: {0}. Seeders: {1}", cur_result.name, cur_result.seeders)
+                                      " minimum seeders: {0}. Seeders: {1}".format(cur_result.name, cur_result.seeders))
                 continue
 
         # check if leechers meet out minimum requirements, disgard result if it does not
         if hasattr(cur_result.provider, 'minleech') and cur_result.leechers not in (-1, None):
             if int(cur_result.leechers) < int(cur_result.provider.minleech):
                 sickrage.app.log.info("Discarding torrent because it doesn't meet the"
-                                      " minimum leechers: {0}. Leechers: {1}", cur_result.name, cur_result.leechers)
+                                      " minimum leechers: {0}. Leechers: {1}".format(cur_result.name, cur_result.leechers))
                 continue
 
         if show.rls_ignore_words and show_names.containsAtLeastOneWord(cur_result.name,
