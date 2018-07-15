f6266197c8bab0fb3e7340e708c63e78fdb60a5c
echel0n <echel0n@sickrage.ca>
2018-07-14 18:03:55 -0700

    Converted provider icons to sprites Converted subtitle icons to sprites Converted network icons to sprites Converted notifier icons to sprites Converted core icons to sprites

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
-// CSS
-require('./scss/core.scss')
+// SCSS/CSS
+require('./scss/core.scss');
+require('./spritesmith-generated/sickrage-core.css');
+require('./spritesmith-generated/sickrage-notifiers.css');
+require('./spritesmith-generated/sickrage-network.css');
+require('./spritesmith-generated/sickrage-providers.css');
+require('./spritesmith-generated/sickrage-subtitles.css');
 
 // JS
-require('./js/core.js')
+require('./js/core.js');
