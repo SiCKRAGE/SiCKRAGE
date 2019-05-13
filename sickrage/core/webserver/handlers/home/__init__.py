80ebdad5e385a2341bdcb379cac815c4d2937998
echel0n <echel0n@sickrage.ca>
2019-05-13 11:11:16 -0700

    Fixed issue with polling for episode search status from scheduler page when no show ID is provided.

--- sickrage/core/webserver/handlers/home/__init__.py
+++ sickrage/core/webserver/handlers/home/__init__.py
@@ -1765,11 +1765,11 @@ class GetManualSearchStatusHandler(BaseHandler, ABC):
     def get_episodes(self, show_id, episode_ids, search_status):
         results = []
 
+        if not show_id:
+            return results
+
         for episode_id in episode_ids:
             episode_obj = find_episode(show_id, episode_id)
-            if not episode_obj:
-                continue
-
             results.append({'show': show_id,
                             'episode': episode_obj.episode,
                             'episodeindexid': episode_id,
