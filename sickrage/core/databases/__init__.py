99b8bb820f00a2a439651ec13ca447232db31e7b
echel0n <echel0n@sickrage.ca>
2019-05-20 18:20:36 -0700

    Refactored `set_sqlite_pragma` function.

--- sickrage/core/databases/__init__.py
+++ sickrage/core/databases/__init__.py
@@ -35,21 +35,21 @@ from sqlalchemy.orm import sessionmaker, mapper
 import sickrage
 
 
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    if not isinstance(dbapi_connection, sqlite3.Connection):
-        return
-
-    old_isolation = dbapi_connection.isolation_level
-    dbapi_connection.isolation_level = None
-    cursor = dbapi_connection.cursor()
-
-    if 'wal' not in cursor.execute("PRAGMA journal_mode").fetchone():
-        cursor.execute("PRAGMA journal_mode=WAL")
-        cursor.execute('PRAGMA busy_timeout=%i;' % 15000)
-
-    cursor.close()
-    dbapi_connection.isolation_level = old_isolation
+# @event.listens_for(Engine, "connect")
+# def set_sqlite_pragma(dbapi_connection, connection_record):
+#     if not isinstance(dbapi_connection, sqlite3.Connection):
+#         return
+#
+#     old_isolation = dbapi_connection.isolation_level
+#     dbapi_connection.isolation_level = None
+#     cursor = dbapi_connection.cursor()
+#
+#     if 'wal' not in cursor.execute("PRAGMA journal_mode").fetchone():
+#         cursor.execute("PRAGMA journal_mode=WAL")
+#         cursor.execute('PRAGMA busy_timeout=%i;' % 15000)
+#
+#     cursor.close()
+#     dbapi_connection.isolation_level = old_isolation
 
 
 @event.listens_for(mapper, "init")
@@ -131,6 +131,13 @@ class srDatabase(object):
 
         self.db_path = os.path.join(sickrage.app.data_dir, '{}.db'.format(self.name))
         self.db_repository = os.path.join(os.path.dirname(__file__), self.name, 'db_repository')
+
+        # enable WAL mode
+        if 'sqlite' in str(self.engine.url):
+            db_connection = self.engine.connect()
+            db_connection.execute('PRAGMA journal_mode=WAL')
+            db_connection.execute('PRAGMA busy_timeout=%i;' % 15000)
+            db_connection.close()
 
         if not self.version:
             api.version_control(self.engine, self.db_repository, api.version(self.db_repository))
