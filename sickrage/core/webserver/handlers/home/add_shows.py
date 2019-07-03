6471a4742483ef2de86795b487d23303f846ed39
echel0n <echel0n@sickrage.ca>
2019-07-02 21:30:49 -0700

    Fixes SICKRAGE-APP-PY3-C6

--- sickrage/core/webserver/handlers/home/add_shows.py
+++ sickrage/core/webserver/handlers/home/add_shows.py
@@ -103,7 +103,7 @@ class SearchIndexersForShowNameHandler(BaseHandler, ABC):
         for i, shows in results.items():
             final_results.extend([[IndexerApi(i).name, i, IndexerApi(i).config["show_url"],
                                    int(show['id']), show['seriesname'], show['firstaired'],
-                                   ('', 'disabled')[find_show(int(show['id'])) is not None]] for show in shows])
+                                   ('', 'disabled')[isinstance(find_show(int(show['id'])), TVShow)]] for show in shows])
 
         lang_id = IndexerApi().indexer().languages[lang] or 7
         return self.write(json_encode({'results': final_results, 'langid': lang_id}))
