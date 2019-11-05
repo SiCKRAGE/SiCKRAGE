650daf44e2408b702e8105cc23b0f1b0c0f87ede
echel0n <echel0n@sickrage.ca>
2019-11-05 07:40:47 -0800

    Revert "Switched to using async sleep when waiting for web socket clients to connect."
    This reverts commit 839e5ae1

--- sickrage/core/websocket/__init__.py
+++ sickrage/core/websocket/__init__.py
@@ -1,7 +1,6 @@
 import json
 from time import sleep
 
-from tornado import gen
 from tornado.websocket import WebSocketHandler
 
 import sickrage
@@ -64,7 +63,8 @@ class WebSocketMessage(object):
     def push(self):
         """Push the message to all connected WebSocket clients."""
         while not clients:
-            gen.sleep(0.1)
+            sleep(0.1)
 
         for client in clients:
+            sleep(0.1)
             sickrage.app.io_loop.add_callback(client.write_message, self.json())
