940d091a70fa50df2896f252fb6ef5279ec5115b
echel0n <echel0n@sickrage.ca>
2019-11-05 07:41:47 -0800

    Revert "Refactored TVShow and TVEpisode database models to use select instead of joined for lazy loading relationships."
    This reverts commit 7bff6d73

--- sickrage/core/tv/episode/__init__.py
+++ sickrage/core/tv/episode/__init__.py
@@ -81,7 +81,7 @@ class TVEpisode(MainDBBase):
     version = Column(Integer, default=-1)
     release_group = Column(Text, default='')
 
-    show = relationship('TVShow', uselist=False, backref='tv_episodes')
+    show = relationship('TVShow', uselist=False, backref='tv_episodes', lazy='joined')
 
     def __init__(self, **kwargs):
         super(TVEpisode, self).__init__(**kwargs)
