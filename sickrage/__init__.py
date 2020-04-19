09556122f82ebce44adcfbd4b2e6b1e83ed40b67
echel0n <echel0n@sickrage.ca>
2020-04-19 09:05:55 -0700

    Fixed path to requirements for startup install of missing modules

--- sickrage/__init__.py
+++ sickrage/__init__.py
@@ -223,7 +223,7 @@ def main():
             from sickrage.core import Core
         except ModuleNotFoundError:
             print('Attempting to install SiCKRAGE missing requirements using pip')
-            subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", REQS_FILE])
             from sickrage.core import Core
 
         # main app instance
