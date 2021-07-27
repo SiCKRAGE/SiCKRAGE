c938a6d0de01acc862ae04f90bf786208e329c33
echel0n <echel0n@sickrage.ca>
2021-07-27 00:46:45 +0000

    [TASK] Pre-Releasing v10.0.12.dev6

--- sickrage/__init__.py
+++ sickrage/__init__.py
@@ -19,7 +19,7 @@
 #  along with SiCKRAGE.  If not, see <http://www.gnu.org/licenses/>.
 # ##############################################################################
 
-__version__ = "10.0.12.dev5"
+__version__ = "10.0.12.dev6"
 __install_type__ = ""
 
 import argparse
