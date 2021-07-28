57a8f505b569a8c8907dfcd27c3c0149a0fe383d
echel0n <echel0n@sickrage.ca>
2021-07-28 15:41:05 +0000

    [TASK] Releasing v10.0.12

--- sickrage/__init__.py
+++ sickrage/__init__.py
@@ -19,7 +19,7 @@
 #  along with SiCKRAGE.  If not, see <http://www.gnu.org/licenses/>.
 # ##############################################################################
 
-__version__ = "10.0.12.dev8"
+__version__ = "10.0.12"
 __install_type__ = ""
 
 import argparse
