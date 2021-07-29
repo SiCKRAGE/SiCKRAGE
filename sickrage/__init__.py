6868e7166ab488560ae031b038b61e2b0d287b56
echel0n <echel0n@sickrage.ca>
2021-07-29 20:59:55 +0000

    [TASK] Releasing v10.0.18

--- sickrage/__init__.py
+++ sickrage/__init__.py
@@ -19,7 +19,7 @@
 #  along with SiCKRAGE.  If not, see <http://www.gnu.org/licenses/>.
 # ##############################################################################
 
-__version__ = "10.0.18.dev1"
+__version__ = "10.0.18"
 __install_type__ = ""
 
 import argparse
