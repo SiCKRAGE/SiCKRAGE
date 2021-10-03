f0db74855dc5c3f6e466aee2dc0aa4b5fb4f4a91
echel0n <echel0n@sickrage.ca>
2021-10-02 19:26:01 -0700

    cleaned up oauth2 offline token migration code

--- sickrage/core/databases/cache/migrations/versions/010_Remove_OAuth2Token_Table.py
+++ sickrage/core/databases/cache/migrations/versions/010_Remove_OAuth2Token_Table.py
@@ -31,20 +31,25 @@ def upgrade():
     with op.get_context().begin_transaction():
         for row in conn.execute(oauth2_token.select()):
             refresh_token = row.refresh_token
+            if refresh_token:
+                break
 
     if refresh_token:
         certs = sickrage.app.auth_server.certs()
-        new_token = sickrage.app.auth_server.refresh_token(refresh_token)
-        decoded_token = sickrage.app.auth_server.decode_token(new_token['access_token'], certs)
-        apikey = decoded_token['apikey']
+        if certs:
+            new_token = sickrage.app.auth_server.refresh_token(refresh_token)
+            if new_token:
+                decoded_token = sickrage.app.auth_server.decode_token(new_token['access_token'], certs)
+                apikey = decoded_token.get('apikey')
 
-        try:
-            session = sickrage.app.config.db.session()
-            general = session.query(ConfigDB.General).one()
-            general.sso_api_key = apikey
-            session.commit()
-        except orm.exc.NoResultFound:
-            pass
+                try:
+                    if apikey:
+                        session = sickrage.app.config.db.session()
+                        general = session.query(ConfigDB.General).one()
+                        general.sso_api_key = apikey
+                        session.commit()
+                except orm.exc.NoResultFound:
+                    pass
 
     if conn.engine.dialect.has_table(conn.engine, 'oauth2_token'):
         op.drop_table('oauth2_token')
