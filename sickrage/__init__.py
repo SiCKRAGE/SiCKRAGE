d0631147bddd93f7083b41887cbd6d4ea4c625e5
echel0n <echel0n@sickrage.ca>
2022-06-18 00:02:52 +0000

    [TASK] Pre-Releasing v10.0.65.dev1

--- sickrage/__init__.py
+++ sickrage/__init__.py
@@ -19,7 +19,7 @@
 #  along with SiCKRAGE.  If not, see <http://www.gnu.org/licenses/>.
 # ##############################################################################
 
-__version__ = "10.0.65.dev0"
+__version__ = "10.0.65.dev1"
 __install_type__ = ""
 
 import sys
